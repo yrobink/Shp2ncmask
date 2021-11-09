bd5ab75ac7e9ebabf336e34d83787eedf92a7a66
Yoann Robin <yoann.robin.k@gmail.com>
2021-11-09 08:36:47 +0100

    version 1.0.0b2

--- Shp2ncmask/__release.py
+++ Shp2ncmask/__release.py
@@ -20,7 +20,7 @@
 version_major = 1
 version_minor = 0
 version_patch = 0
-version_extra = "b1"
+version_extra = "b2"
 version       = "{}.{}.{}{}".format(version_major,version_minor,version_patch,version_extra)
 
 
